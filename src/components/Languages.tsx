43a4a4d768d919711857a08dd22577d8f9089295
Mark Cornelio <markcornelio28@gmail.com>
2024-06-05 14:32:53 +0800

    fix the PL display on mobile

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -1,35 +1,35 @@
 export default function Languages() {
   return (
     <section className="my-10 text-6xl text-cyan-800 text-center">
-      <div className="flex flex-wrap justify-between lg:mb-20">
-        <i className="devicon-php-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-laravel-original mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-lumen-original mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-javascript-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-react-original mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-vuejs-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-angularjs-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-livewire-plain-wordmark mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
+      <div className="grid grid-cols-4 gap-4 lg:flex lg:flex-row justify-between lg:mb-20">
+        <i className="devicon-php-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-laravel-original mb-4 lg:mb-0"></i>
+        <i className="devicon-lumen-original mb-4 lg:mb-0"></i>
+        <i className="devicon-javascript-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-react-original mb-4 lg:mb-0"></i>
+        <i className="devicon-vuejs-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-angularjs-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-livewire-plain-wordmark mb-4 lg:mb-0"></i>
       </div>
-      <div className="flex flex-wrap justify-between lg:mb-20">
-        <i className="devicon-css3-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-tailwindcss-original mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-digitalocean-original mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-arduino-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-amazonwebservices-plain-wordmark mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-git-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-codeigniter-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-electron-original mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
+      <div className="grid grid-cols-4 gap-4 lg:flex lg:flex-row justify-between lg:mb-20">
+        <i className="devicon-css3-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-tailwindcss-original mb-4 lg:mb-0"></i>
+        <i className="devicon-digitalocean-original mb-4 lg:mb-0"></i>
+        <i className="devicon-arduino-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-amazonwebservices-plain-wordmark mb-4 lg:mb-0"></i>
+        <i className="devicon-git-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-codeigniter-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-electron-original mb-4 lg:mb-0"></i>
       </div>
-      <div className="flex flex-wrap justify-between lg:mb-20">
-        <i className="devicon-docker-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-jira-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-digitalocean-original mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-html5-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-mysql-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-nextjs-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-alpinejs-original mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
-        <i className="devicon-cplusplus-plain mb-4 lg:mb-0 w-1/2 sm:w-auto"></i>
+      <div className="grid grid-cols-4 gap-4 lg:flex lg:flex-row justify-between lg:mb-20">
+        <i className="devicon-docker-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-jira-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-digitalocean-original mb-4 lg:mb-0"></i>
+        <i className="devicon-html5-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-mysql-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-nextjs-plain mb-4 lg:mb-0"></i>
+        <i className="devicon-alpinejs-original mb-4 lg:mb-0"></i>
+        <i className="devicon-cplusplus-plain mb-4 lg:mb-0"></i>
       </div>
     </section>
   );
