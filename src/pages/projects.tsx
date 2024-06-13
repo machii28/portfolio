235f730246b01bfeaf99ac724baadf904f68578d
Mark Cornelio <markcornelio28@gmail.com>
2024-06-14 00:26:57 +0800

    added new fix

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,23 +1,24 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Projects() {
   return (
     <main className="w-full mx-auto max-w-screen-2xl py-6 px-16">
-      <Navbar />
+      <Navbar/>
 
       <section className="lg:my-20">
 
         <div className="flex flex-col lg:flex-row justify-between my-16">
           <div className="mx-auto lg:mx-0 p-10 lg:mb-0 mb-5 shadow lg:w-1/2">
-            <Image src={'/projects/SurveyGizmo.png'} alt={'SurveyGizmo'} width={700} height={0} />
+            <Image src={'/projects/SurveyGizmo.png'} alt={'SurveyGizmo'} width={700} height={0}/>
           </div>
           <div className="text-center lg:text-left mb-20 lg:mb-0 px-10 lg:w-1/2">
-            <h1 className="font-bold text-gray-800 text-2xl md:text-3xl xl:text-4xl block">
+            <h1 className="font-bold text-cyan-600 text-2xl md:text-3xl xl:text-4xl block">
               Laravel Survey Gizmo
             </h1>
-            <span className="font-normal inline-block text-gray-400">PHP Composer Package</span>
+            <span className="font-normal inline-block text-cyan-500">PHP Composer Package</span>
             <p className="font-normal text-gray-500 text-sm md:text-md xl:text-lg mt-5 mb-10">
               A PHP Library that wraps the API of SurveyGizmo (now named as Alchemer)
             </p>
@@ -26,13 +27,13 @@ export default function Projects() {
 
         <div className="flex flex-col lg:flex-row justify-between my-16">
           <div className="mx-auto lg:mx-0 p-10 lg:mb-0 mb-5 shadow lg:w-1/2">
-            <Image src={'/projects/Shopify-client.png'} alt={'Shopify Client'} width={700} height={0} />
+            <Image src={'/projects/Shopify-client.png'} alt={'Shopify Client'} width={700} height={0}/>
           </div>
           <div className="text-center lg:text-left lg:mb-0 px-10 lg:w-1/2">
-            <h1 className="font-bold text-gray-800 text-2xl md:text-3xl xl:text-4xl block">
+            <h1 className="font-bold text-cyan-600 text-2xl md:text-3xl xl:text-4xl block">
               Shopify Client
             </h1>
-            <span className="font-normal inline-block text-gray-400">PHP Composer Package</span>
+            <span className="font-normal inline-block text-cyan-500">PHP Composer Package</span>
             <p className="font-normal text-gray-500 text-sm md:text-md xl:text-lg mt-5 mb-10">
               A PHP Library that provides a fluent implementation of Shopify API.
               It&apos;s built on top osiset/Basic-Shopify-API.
@@ -42,13 +43,33 @@ export default function Projects() {
 
         <div className="flex flex-col lg:flex-row justify-between my-16">
           <div className="mx-auto lg:mx-0 p-10 lg:mb-0 mb-5 shadow lg:w-1/2">
-            <Image src={'/projects/Cyclista.png'} alt={'Shopify Client'} width={700} height={0} />
+            <Link href={'https://pokedex-8hzq-git-main-mark-cornelios-projects-bd6166d2.vercel.app'}>
+              <Image src={'/projects/Pokedex.png'} alt={'Shopify Client'} width={700} height={0}/>
+            </Link>
           </div>
           <div className="text-center lg:text-left lg:mb-0 px-10 lg:w-1/2">
-            <h1 className="font-bold text-gray-800 text-2xl md:text-3xl xl:text-4xl block">
+            <Link href='https://pokedex-8hzq-git-main-mark-cornelios-projects-bd6166d2.vercel.app/#'
+                  className="font-bold text-cyan-600 text-2xl md:text-3xl xl:text-4xl block">
+              Pokemon App
+            </Link>
+            <span className="font-normal inline-block text-cyan-500">Mobile App</span>
+            <p className="font-normal text-gray-500 text-sm md:text-md xl:text-lg mt-5 mb-10">
+              A Pokemon app that is built using React/Next JS. It&apos;s still an ongoing project with
+              the vision of implementing a Python backend with an integration of LLM. For building a
+              pokemon competitive team
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col lg:flex-row justify-between my-16">
+          <div className="mx-auto lg:mx-0 p-10 lg:mb-0 mb-5 shadow lg:w-1/2">
+            <Image src={'/projects/Cyclista.png'} alt={'Shopify Client'} width={700} height={0}/>
+          </div>
+          <div className="text-center lg:text-left lg:mb-0 px-10 lg:w-1/2">
+            <h1 className="font-bold text-cyan-600 text-2xl md:text-3xl xl:text-4xl block">
               Cyclista
             </h1>
-            <span className="font-normal inline-block text-gray-400">Mobile App</span>
+            <span className="font-normal inline-block text-cyan-500">Mobile App</span>
             <p className="font-normal text-gray-500 text-sm md:text-md xl:text-lg mt-5 mb-10">
               A Bike app that&apos;s built using Vue and Lumen. With an integration of the leaflet JS
             </p>
@@ -57,13 +78,13 @@ export default function Projects() {
 
         <div className="flex flex-col lg:flex-row justify-between my-16 sm:my-0">
           <div className="mx-auto lg:mx-0 p-10 lg:mb-0 mb-5 shadow lg:w-1/2">
-            <Image src={'/projects/Daparoo.png'} alt={'Shopify Client'} width={700} height={0} />
+            <Image src={'/projects/Daparoo.png'} alt={'Shopify Client'} width={700} height={0}/>
           </div>
           <div className="text-center lg:text-left lg:mb-0 px-10 lg:w-1/2">
-            <h1 className="font-bold text-gray-800 text-2xl md:text-3xl xl:text-4xl block">
-              DAPAROO
+            <h1 className="font-bold text-cyan-600 text-2xl md:text-3xl xl:text-4xl block">
+              Daparoo
             </h1>
-            <span className="font-normal inline-block text-gray-400">Ecommerce Web App</span>
+            <span className="font-normal inline-block text-cyan-500">Ecommerce Web App</span>
             <p className="font-normal text-gray-500 text-sm md:text-md xl:text-lg mt-5 mb-10">
               An ecommerce website that is built using Laravel and Vue JS.
             </p>
@@ -72,13 +93,13 @@ export default function Projects() {
 
         <div className="flex flex-col lg:flex-row justify-between my-16">
           <div className="mx-auto lg:mx-0 p-10 lg:mb-0 mb-5 shadow lg:w-1/2">
-            <Image src={'/projects/LingoChingoo.png'} alt={'Shopify Client'} width={700} height={0} />
+            <Image src={'/projects/LingoChingoo.png'} alt={'Shopify Client'} width={700} height={0}/>
           </div>
           <div className="text-center lg:text-left lg:mb-0 px-10 lg:w-1/2">
-            <h1 className="font-bold text-gray-800 text-2xl md:text-3xl xl:text-4xl block">
+            <h1 className="font-bold text-cyan-600 text-2xl md:text-3xl xl:text-4xl block">
               LingoChingoo
             </h1>
-            <span className="font-normal inline-block text-gray-400">Social Networking App</span>
+            <span className="font-normal inline-block text-cyan-500">Social Networking App</span>
             <p className="font-normal text-gray-500 text-sm md:text-md xl:text-lg mt-5 mb-10">
               A Social Networking Web App that is built using Laravel and Vue JS
             </p>
@@ -87,13 +108,13 @@ export default function Projects() {
 
         <div className="flex flex-col lg:flex-row justify-between my-16">
           <div className="mx-auto lg:mx-0 p-10 lg:mb-0 mb-5 shadow lg:w-1/2">
-            <Image src={'/projects/TAC.png'} alt={'Shopify Client'} width={700} height={0} />
+            <Image src={'/projects/TAC.png'} alt={'Shopify Client'} width={700} height={0}/>
           </div>
           <div className="text-center lg:text-left lg:mb-0 px-10 lg:w-1/2">
-            <h1 className="font-bold text-gray-800 text-2xl md:text-3xl xl:text-4xl block">
+            <h1 className="font-bold text-cyan-600 text-2xl md:text-3xl xl:text-4xl block">
               Thera Game with Arduino Glove Controller
             </h1>
-            <span className="font-normal inline-block text-gray-400">Unity Game and Arduino Device</span>
+            <span className="font-normal inline-block text-cyan-500">Unity Game and Arduino Device</span>
             <p className="font-normal text-gray-500 text-sm md:text-md xl:text-lg mt-5 mb-10">
               A Glove controller that enables the user to control a unity game.
             </p>
@@ -102,7 +123,7 @@ export default function Projects() {
 
       </section>
 
-      <Footer />
+      <Footer/>
     </main>
   );
 }
